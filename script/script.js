639d039dc10a9e216d636ff4514468356859d5af
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-02-28 15:58:18 +0100

    generating random question in ui

--- script/script.js
+++ script/script.js
@@ -1,25 +1,49 @@
-let data = []
-async function fetchQuestions() {
-  try {
-    const response = await fetch("questions.json")
-    data = await response.json()
-    data = data.questions.question
-    generateRandomQuestion()
-  }
-  catch (error) {
-    console.log(error)
-  }
-}
+// let data = []
+// async function fetchQuestions() {
+//   try {
+//     const response = await fetch("questions.json")
+//     data = await response.json()
+//     data = data.questions.question
+//     generateRandomQuestion()
+//   }
+//   catch (error) {
+//     console.log(error)
+//   }
+// }
 
-function generateRandomQuestion() {
-  let randomQuestion = data[(Math.floor(Math.random() * (data.length)))]
-  console.log(randomQuestion)
-  showQuestionUI(randomQuestion)
-}
+// function generateRandomQuestion() {
+//   let randomQuestion = data[(Math.floor(Math.random() * (data.length)))]
+//   console.log(randomQuestion)
+//   showQuestionUI(randomQuestion)
+// }
 
-function showQuestionUI(randomQuestion) {
-  let questionEl = document.querySelector(".question")
-  questionEl.innerHTML = randomQuestion.question
-}
+// function showQuestionUI(randomQuestion) {
+//   let questionEl = document.querySelector(".question")
+//   questionEl.innerHTML = randomQuestion.question
+// }
 
-fetchQuestions()
+// let formEl = document.querySelector("form")
+// formEl.addEventListener("submit", getAnswers)
+
+
+
+// function getAnswers(event) {
+//   event.preventDefault()
+//   const input = new FormData(event.target)
+//   const name = input.get('name')
+//   const answer = input.get('answer')
+//   saveAnswersToJson(name, answer)
+// }
+
+// function saveAnswersToJson(name, answer) {
+//   class Answers {
+//     constructor (name, answer) {
+//       this.name = name;
+//       this.answer = answer;
+//     }}
+//     const savedAnswer = new Answers()
+// }
+
+console.log("hej")
+
+// fetchQuestions()
