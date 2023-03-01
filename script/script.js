71a62b71b0112a68e36cbc91fa668b2da81840a1
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-03-01 11:24:04 +0100

    quick push

--- script/script.js
+++ script/script.js
@@ -1,54 +1,3 @@
-<<<<<<< Updated upstream
-// let data = []
-// async function fetchQuestions() {
-//   try {
-//     const response = await fetch("questions.json")
-//     data = await response.json()
-//     data = data.questions.question
-//     generateRandomQuestion()
-//   }
-//   catch (error) {
-//     console.log(error)
-//   }
-// }
-
-// function generateRandomQuestion() {
-//   let randomQuestion = data[(Math.floor(Math.random() * (data.length)))]
-//   console.log(randomQuestion)
-//   showQuestionUI(randomQuestion)
-// }
-
-// function showQuestionUI(randomQuestion) {
-//   let questionEl = document.querySelector(".question")
-//   questionEl.innerHTML = randomQuestion.question
-// }
-
-// let formEl = document.querySelector("form")
-// formEl.addEventListener("submit", getAnswers)
-
-
-
-// function getAnswers(event) {
-//   event.preventDefault()
-//   const input = new FormData(event.target)
-//   const name = input.get('name')
-//   const answer = input.get('answer')
-//   saveAnswersToJson(name, answer)
-// }
-
-// function saveAnswersToJson(name, answer) {
-//   class Answers {
-//     constructor (name, answer) {
-//       this.name = name;
-//       this.answer = answer;
-//     }}
-//     const savedAnswer = new Answers()
-// }
-
-console.log("hej")
-
-// fetchQuestions()
-=======
 let data = [];
 
 async function fetchQuestions() {
@@ -56,6 +5,7 @@ async function fetchQuestions() {
     const response = await fetch("questions.json");
     data = await response.json();
     data = data.questions.question;
+
     generateRandomQuestion();
   } catch (error) {
     console.log(error);
@@ -63,15 +13,57 @@ async function fetchQuestions() {
 }
 
 function generateRandomQuestion() {
-  let randomQuestion = data[Math.floor(Math.random() * data.length)].question;
+  let randomQuestion = data[Math.floor(Math.random() * data.length)];
 
   showQuestionUI(randomQuestion);
 }
 
 function showQuestionUI(randomQuestion) {
   let questionEl = document.querySelector(".question");
-  questionEl.innerHTML = randomQuestion;
+  questionEl.innerHTML = randomQuestion.question;
 }
 
+let formEl = document.querySelector("form");
+formEl.addEventListener("submit", getAnswers);
+
+function getAnswers(event) {
+  event.preventDefault();
+  const input = new FormData(event.target);
+  const name = input.get("name");
+  const answer = input.get("answer");
+
+  checkResult(name, answer);
+
+  // saveAnswersToJson(name, answer);
+}
+
+function checkResult(name, answer) {
+  let question = document.querySelector(".question").innerHTML;
+  console.log(question);
+}
+
+// function saveAnswersToJson(name, answer) {
+//   class Answers {
+//     constructor(name, answer) {
+//       this.name = name;
+//       this.answer = answer;
+//     }
+//   }
+//   const savedAnswer = new Answers(name, answer);
+
+//   // const fs = require("fs");
+//   // let answerFile = fs.readFileSync("answers.json");
+//   // parsedAnswerFile = JSON.parse(answerFile);
+//   // console.log(parsedAnswerFile);
+//   // savedAnswerToFile = JSON.stringify(savedAnswer);
+//   // fs.writeFileSync("answers.json", savedAnswerToFile, (err) => {
+//   //   if (err) throw err;
+//   //   console.log("new data added");
+//   // });
+// }
+
+// function showResult(name, answer) {
+//   if(answer ==)
+// }
+
 fetchQuestions();
->>>>>>> Stashed changes
